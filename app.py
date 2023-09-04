a6000d0a6a5c828af5793c1082a44fd0d4565c77
Benedikt Fassian <mbf@fassian.com>
2023-09-04 09:30:18 +0200

    feat: added metris route

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index():
+    return "Hello World!"
+
+@app.route('/metrics')
+def metrics():
+    return "broker_topic_1 12"

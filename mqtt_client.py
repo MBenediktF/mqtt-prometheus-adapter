eeb74ef3b33cfc20f784a601fc93ba54f7e08979
Benedikt Fassian <mbf@fassian.com>
2023-09-04 15:27:04 +0200

    fix: added threading to run mqtt client loop

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import sys
+import threading
 
 class MQTTClient:
     def __init__(self, host, port, topics):
@@ -15,7 +16,8 @@ class MQTTClient:
             print(f"Error connecting to host: {e}")
             sys.exit(1)
 
-        client.loop_forever()
+        mqtt_loop_thread = threading.Thread(target=client.loop_forever)
+        mqtt_loop_thread.start()
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
@@ -34,7 +36,7 @@ class MQTTClient:
         for topic in self.topics:
             if topic['path'] == msg.topic:
                 topic['value'] = msg.payload
+                break
 
-
-    def getData():
-        return []
+    def getData(self):
+        return self.topics

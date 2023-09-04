835b225f7557eb2096cd121f26db9662b6952a2a
Benedikt Fassian <mbf@fassian.com>
2023-09-04 13:42:17 +0200

    refacotor: Added topics, changed order, renamed host

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ import sys
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "MQTT-Prometheus-Adapter is running on this port. <br> Go to the <a href='/metrics'>metrics</a>"
-
-@app.route('/metrics')
-def metrics():
-    return "broker_topic_1 12"
-
 try:
     with open(config_file_path, 'r') as config_file:
         config = yaml.safe_load(config_file)
@@ -26,9 +18,9 @@ except yaml.YAMLError as e:
     print(f'Error: Could not read config file: {e}')
     sys.exit(1)
 
-endpoint = config.get('endpoint', False)
-if not endpoint:
-    print("Error: Endpoint not specified in config")
+host = config.get('host', False)
+if not host:
+    print("Error: Host not specified in config")
     sys.exit(1)
 
 port = config.get('port', False)
@@ -36,7 +28,20 @@ if not port:
     print("Error: Port not specified in config")
     sys.exit(1)
 
-print(f'Endpoint: {endpoint}')
+topics = config.get('topics', [])
+
+print(str(topics))
+
+print(f'Host: {host}')
 print(f'Port: {port}')
 
-mqtt = MQTTClient(endpoint, port)
+mqtt = MQTTClient(host, port, topics)
+
+@app.route('/')
+def index():
+    return "MQTT-Prometheus-Adapter is running on this port. <br> Go to the <a href='/metrics'>metrics</a>"
+
+@app.route('/metrics')
+def metrics():
+    data = mqtt.getData()
+    return "broker_topic_1 12"
--- mqtt_client.py
+++ mqtt_client.py
@@ -1,18 +1,25 @@
 import paho.mqtt.client as mqtt
 
 class MQTTClient:
-    def __init__(self, endpoint, port):
+    def __init__(self, host, port, topics):
+        self.topics = topics
         client = mqtt.Client()
         client.on_connect = self.on_connect
         client.on_message = self.on_message
-        client.connect(endpoint, port, 60)
 
+        client.connect(host, port)
+        print("Test")
         client.loop_forever()
 
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
 
-        client.subscribe("")
+        for topic in self.topics:
+            print(topic.path)
+            client.subscribe(topic.path)
 
     def on_message(client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
+
+    def getData():
+        return []

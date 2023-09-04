78d313e62a85b00ac29f176ff865e96f1dfc5b51
Benedikt Fassian <mbf@fassian.com>
2023-09-04 10:46:35 +0200

    feat: config file

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
+import yaml
+config_file_path = './mqtt-prometheus-adapter/config.yml'
+
+import sys
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "MQTT-Prometheus-Adapter is running on this port. <br> Go to the <a href='/metrics'>metrics</a>"
 
 @app.route('/metrics')
 def metrics():
     return "broker_topic_1 12"
 
-app.run(port=4444)
+try:
+    with open(config_file_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+except FileNotFoundError:
+    print("Error: Could not open config file.")
+    sys.exit(1)
+except yaml.YAMLError as e:
+    print(f'Error: Could not read config file: {e}')
+    sys.exit(1)
+
+endpoint = config.get('endpoint', False)
+if not endpoint:
+    print("Error: Endpoint not specified in config")
+    sys.exit(1)
+
+print("Endpoint: " + endpoint)
